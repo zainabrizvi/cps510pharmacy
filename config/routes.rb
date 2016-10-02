381c8cc610306a0d130e8e0c39d40544d1c03798
Samuel Dindyal <sam.dindyal@me.com>
2016-10-02 07:33:06 -0400

    Procfile and static pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+
+  root 'static_pages#home'
+
+  get 'about' => 'static_pages#about'
+  get 'help' => 'static_pages#help'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
